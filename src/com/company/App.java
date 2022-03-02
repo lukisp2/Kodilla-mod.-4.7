5d4f7295b13c24a21ed7fe3a535e848529e365f9
Lukasz MARCHEL <lukaszmarchelelk@gmail.com>
2022-03-02 18:36:39 +0100

    Łukasz commit spotkanie

--- src/com/company/App.java
+++ src/com/company/App.java
@@ -5,6 +5,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("Another hello");
+        System.out.println("Another hello 123");
     }
 
 }
